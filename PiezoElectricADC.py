3943de145e569c961e7eba13740ec0f9f8db98ed
Diego <d.marquezp92@gmail.com>
2019-10-09 13:01:53 -0400

    Implementation of PiezoElectric ADC converted + mock results

---
+++ PiezoElectricADC.py
@@ -0,0 +1,23 @@
+class PiezoElectricADC:
+
+  '''
+  Detects the presence based on GPIO readings and processing
+  '''
+  def detectPresence(self):
+    gpioReading = self.readFromADC()
+    detected = self.processADCReading(gpioReading)
+    return detected
+
+  '''
+  Gets the reading from the ADC GPIO pin
+  '''
+  def readFromADC(self):
+    #mock return
+    return 512
+
+  '''
+  Processes the GPIO reading and determines if presence was detected
+  '''
+  def processADCReading(self, gpioReading):
+    #mock return
+    return True
