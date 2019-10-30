92db8256d5db32ac85a0de50fd605aa5bc136d12
Diego <d.marquezp92@gmail.com>
2019-10-30 19:20:24 -0400

    Configuration of IDE

--- PhoneApp/src/pages/Details.tsx
+++ PhoneApp/src/pages/Details.tsx
@@ -6,8 +6,10 @@ import axios from 'axios';
 
 function Details() {
 
+    //Use React Hook to modify the state containing the rawdata from the server
     const [data, setData ] = useState({rawdata : []})
 
+    //AJAX call to obtain the rawdata entries from the raspberry pi
     useAsyncEffect(async () => {
         const result = await axios(`http://${Constants.Constants.SERVER_URL}/rawdata`);
         setData(result.data);
