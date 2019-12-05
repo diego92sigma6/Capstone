69341555d74e72e8184ad7f9f32b7574a01a4982
dmarquez <d.marquezp92@gmail.com>
2019-12-05 14:31:17 -0500

    xxx

--- Main.py
+++ Main.py
@@ -4,18 +4,21 @@ import threading
 from ReadingsHUB import *
 
 
-def output_reader(proc):
+def output_reader(proc, tag):
     for line in iter(proc.stdout.readline, b''):
         #print('got line: {0}'.format(line.decode('utf-8')), end='')
         line = line.replace('\n','')
-        print('[REST] {0}'.format(line.decode('utf-8')))
+        print('{1} {0}'.format(line.decode('utf-8'), tag))
 
 def main():
 
     print("starting REST API")
     #REST API
     proc = subprocess.Popen(['python', '-u', './RestAPI.py'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    t = threading.Thread(target=output_reader, args=(proc,))
+    proc2 = subprocess.Popen(['npm', 'start',  '--prefix' , '/home/pi/Capstone/webapp'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    t2 = threading.Thread(target=output_reader, args=(proc2, '[WEB]'))
+    t2.start()
+    t = threading.Thread(target=output_reader, args=(proc, '[REST]'))
     t.start()
 
     print("starting system")
